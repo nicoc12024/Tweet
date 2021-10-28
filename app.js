b386631d4eb27bf6c332bfd6587976166defe3d9
Nicolas <56944680+nicoc12024@users.noreply.github.com>
2021-10-28 16:21:18 +0200

    main

---
+++ app.js
@@ -0,0 +1,36 @@
+const input = document.querySelector("textarea");
+const btn = document.querySelector(".btn");
+const limit = document.querySelector(".limit");
+let max = 50;
+
+const updateLimit = () => {
+  limit.textContent = max;
+  input.addEventListener("input", () => {
+    let inputLength = input.value.length;
+    limit.textContent = max - inputLength;
+    if (inputLength > max) {
+      btn.disabled = true;
+      limit.style.color = "red";
+      btn.style.backgroundColor = "gray";
+      btn.style.cursor = "default";
+    } else {
+      btn.disabled = false;
+      limit.style.color = "black";
+      btn.style.backgroundColor = "rgb(44, 178, 255)";
+      btn.style.cursor = "pointer";
+    }
+  });
+};
+
+updateLimit();
+
+// Tweet Function
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  tweet();
+});
+
+const tweet = () => {
+  const tweetInput = "https://twitter.com/intent/tweet?text=";
+  window.open(`${tweetInput}${input.value}`);
+};
